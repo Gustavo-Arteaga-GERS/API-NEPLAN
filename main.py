129cc4458ba2d9ae6b998cd7e035c9d3fb3969d0
Gustavo Arteaga <82400391+Gustavo-Arteaga-GERS@users.noreply.github.com>
2023-05-16 20:19:05 -0500

    change for 5

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def neplan():
     attributeName_2 = "Pset"
     attributeName_3 = "Pset"
 
-    for i in range(0, 10):
+    for i in range(0, 5):
 
         neplanservice.SetElementAttributeByID(project, elementID_1, attributeName_1, Dem[i])
         neplanservice.SetElementAttributeByID(project, elementID_2, attributeName_2, Pv1kw[i])
